62317925b41853b71dae33099d0177bec91320af
juliap <juliap@ciandt.com>
2021-11-19 15:44:14 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController
+{
+    public function index() {
+        return Serie::all();
+    }
+}
